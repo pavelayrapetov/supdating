9cfd9cd13a602014c35f1f38dd081f3e9984c38c
Павел Айрапетов <pavelayrapetov@MacBook-Pro-Pavel.local>
2026-02-27 09:27:41 +0500

    Добавлены типы для Telegram.WebApp и MainButton

--- src/telegram.d.ts
+++ src/telegram.d.ts
@@ -14,15 +14,39 @@ interface TelegramWebAppInitDataUnsafe {
   user?: TelegramWebAppUser;
 }
 
+interface BottomButton {
+  text: string;
+  color: string;
+  textColor: string;
+  isVisible: boolean;
+  isActive: boolean;
+  hasShineEffect: boolean;
+  isProgressVisible: boolean;
+
+  setText(text: string): BottomButton;
+  show(): BottomButton;
+  hide(): BottomButton;
+  enable(): BottomButton;
+  disable(): BottomButton;
+  showProgress(leaveActive?: boolean): BottomButton;
+  hideProgress(): BottomButton;
+  setParams(params: {
+    text?: string;
+    color?: string;
+    text_color?: string;
+    has_shine_effect?: boolean;
+    is_active?: boolean;
+    is_visible?: boolean;
+  }): BottomButton;
+  onClick(callback: () => void): void;
+  offClick(callback: () => void): void;
+}
+
 interface TelegramWebApp {
   ready: () => void;
   expand: () => void;
   initDataUnsafe: TelegramWebAppInitDataUnsafe;
-  MainButton: {
-    setText: (text: string) => void;
-    show: () => void;
-    hide: () => void;
-  };
+  MainButton: BottomButton;
 }
 
 interface Window {
